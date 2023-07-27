7ca2ba3490de04c025f6210b22de5fd73316e957
Eugenia Caceres <eugeniacaceres.mh@gmail.com>
2023-07-27 01:39:46 -0300

    cambios en el estilo y boton hamburgesa

--- src/components/common/CustomNav.jsx
+++ src/components/common/CustomNav.jsx
@@ -16,54 +16,59 @@ const CustomNav = ({ usuarioLogueado, setUsuarioLogueado }) => {
   return (
     <>
       {/* Imagen centrada arriba de la barra de navegación */}
-      <div className="text-center mt-3">
+      <div className="text-center mt-4">
         {/* Enlace a la página de inicio */}
-        <Link to="/">
-          <Image
-            src={logo}
-            alt="Logo"
-            style={{ width: '5%', height: '5%', objectFit: 'relative' }}
-            className="mb-3"
-          />
+        <Link to="home">
+          <Image src={logo} alt="Logo" className="logo" />
         </Link>
       </div>
+      <Container className="mb-4 d-flex justify-content-center align-items-center">
+        <Navbar.Brand href="#home" className="tituloNav fs-4 fw-bold">
+        <span className="brand-text">Rolling Restaurant</span>        </Navbar.Brand>
+      </Container>
       {/* Barra de navegación */}
-      <Navbar  expand="lg" className="mx-5 Navbar justify-content-between">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav " />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <NavLink end className={"nav-item nav-link"} to={"/pedidos"}>
-              Pedidos
-            </NavLink>
-            {usuarioLogueado.nombreUsuario ? (
-              <>
+      <Navbar expand="lg" className="mx-5 mb-1 Navbar ">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav " />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <NavLink end className={"nav-item nav-link fs-5"} to={"/pedidos"}>
+                Pedidos
+              </NavLink>
+              {usuarioLogueado.nombreUsuario ? (
+                <>
+                  <NavLink
+                    end
+                    className={"nav-item nav-link fs-5 fw-bold"}
+                    to={"/administrador"}
+                  >
+                    Administrador
+                  </NavLink>
+                  <Button variant="dark" onClick={logout}>
+                    Logout
+                  </Button>
+                </>
+              ) : (
                 <NavLink
                   end
-                  className={"nav-item nav-link"}
-                  to={"/administrador"}
+                  className={"nav-item nav-link fs-5"}
+                  to={"/login"}
                 >
-                  Administrador
+                  Login
                 </NavLink>
-                <Button variant="dark" onClick={logout}>
-                  Logout
-                </Button>
-              </>
-            ) : (
-              <NavLink end className={"nav-item nav-link"} to={"/login"}>
-                Login
+              )}
+
+              <NavLink
+                end
+                className={"nav-item nav-link fs-5"}
+                to={"/registro"}
+              >
+                Registro
               </NavLink>
-            )}
-
-            <NavLink end className={"nav-item nav-link"} to={"/registro"}>
-              Registro
-            </NavLink>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
