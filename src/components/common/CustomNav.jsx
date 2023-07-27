0edc0399a820f8630b16e0f3130ed89049061552
Eugenia Caceres <eugeniacaceres.mh@gmail.com>
2023-07-27 16:42:14 -0300

    agregue el navar al registro y al login

--- src/components/common/CustomNav.jsx
+++ src/components/common/CustomNav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, Container, Nav, Button, Image } from "react-bootstrap";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import logo from "../../assets/trace.svg";
+import logo from "../../assets/trace.svg"; // Importa tu imagen aquí
 import "./CustomNav.css";
 
 const CustomNav = ({ usuarioLogueado, setUsuarioLogueado }) => {
@@ -16,59 +16,54 @@ const CustomNav = ({ usuarioLogueado, setUsuarioLogueado }) => {
   return (
     <>
       {/* Imagen centrada arriba de la barra de navegación */}
-      <div className="text-center mt-4">
+      <div className="text-center mt-3">
         {/* Enlace a la página de inicio */}
-        <Link to="/home">
-          <Image src={logo} alt="Logo" className="logo" />
+        <Link to="/">
+          <Image
+            src={logo}
+            alt="Logo"
+            style={{ width: '5%', height: '5%', objectFit: 'relative' }}
+            className="mb-3"
+          />
         </Link>
       </div>
-      <Container className="mb-4 d-flex justify-content-center align-items-center">
-        <Navbar.Brand href="/home" className="tituloNav fs-4 fw-bold">
-        <span className="brand-text">Rolling Restaurant</span>        </Navbar.Brand>
-      </Container>
       {/* Barra de navegación */}
-      <Navbar expand="lg" className="mx-5 mb-1 Navbar ">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav " />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <NavLink end className={"nav-item nav-link fs-5"} to={"/pedidos"}>
-                Pedidos
-              </NavLink>
-              {usuarioLogueado.nombreUsuario ? (
-                <>
-                  <NavLink
-                    end
-                    className={"nav-item nav-link fs-5 fw-bold"}
-                    to={"/administrador"}
-                  >
-                    Administrador
-                  </NavLink>
-                  <Button variant="dark" onClick={logout}>
-                    Logout
-                  </Button>
-                </>
-              ) : (
+      <Navbar  expand="lg" className="mx-5 Navbar justify-content-between">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav " />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink end className={"nav-item nav-link"} to={"/pedidos"}>
+              Pedidos
+            </NavLink>
+            {usuarioLogueado.nombreUsuario ? (
+              <>
                 <NavLink
                   end
-                  className={"nav-item nav-link fs-5"}
-                  to={"/login"}
+                  className={"nav-item nav-link"}
+                  to={"/administrador"}
                 >
-                  Login
+                  Administrador
                 </NavLink>
-              )}
-
-              <NavLink
-                end
-                className={"nav-item nav-link fs-5"}
-                to={"/registro"}
-              >
-                Registro
+                <Button variant="dark" onClick={logout}>
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <NavLink end className={"nav-item nav-link"} to={"/login"}>
+                Login
               </NavLink>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+            )}
+
+            <NavLink end className={"nav-item nav-link"} to={"/registro"}>
+              Registro
+            </NavLink>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
     </>
   );
 };
