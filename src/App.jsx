81391e5eda5f1278d9ee33907ccec949134b2f41
EugeniaCaceres <121511003+EugeniaCaceres@users.noreply.github.com>
2023-07-27 15:50:09 -0300

    Revert "agregue navar brand y detalles en el estilo"

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,12 @@
-import { createBrowserRouter, RouterProvider,Route,Routes, BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import Acercadenosotros from "./components/views/Acercadenosotros"
-import Administrador from "./components/views/Administrador";
-import Login from "./components/autenticacion/Login.jsx";
-import Registro from "./components/autenticacion/Registro.jsx";
-import { useState } from "react";
-import ErrorPage from "./components/views/Error404"
-import RutasDelAdmin from "./components/routes/RutasDelAdmin";
-
+import './App.css'
 
 function App() {
-  const usuario = JSON.parse(sessionStorage.getItem('usuario')) || {}; 
-  const [usuarioLogueado, setUsuarioLogueado] = useState(usuario);
-
-
-  const router = createBrowserRouter([
-    
-    {
-      path: "/administrador",
-      element: <Administrador />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/login",
-      element: <Login setUsuarioLogueado={setUsuarioLogueado}></Login>,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/home",
-      element: <Administrador />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/registro",
-      element: <Registro setUsuarioLogueado={setUsuarioLogueado} />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/administrador/*",
-      element: <RutasDelAdmin setUsuarioLogueado={setUsuarioLogueado} />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/acercadenosotros",
-      element: <Acercadenosotros />,
-      errorElement: <ErrorPage />,
-    },
-  ]);
 
   return (
     <>
-      <RouterProvider router={router} />
+      
     </>
-  );
+  )
 }
 
-export default App;
+export default App
