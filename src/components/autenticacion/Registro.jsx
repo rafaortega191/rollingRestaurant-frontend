c5e47f28c115b0e7d47713c9df22fd40e1008198
Eugenia Caceres <eugeniacaceres.mh@gmail.com>
2023-07-30 02:58:17 -0300

    agregue nav a registro y login

--- src/components/autenticacion/Registro.jsx
+++ src/components/autenticacion/Registro.jsx
@@ -9,8 +9,8 @@ import {
   AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
+import CustomNav from "../common/CustomNav.jsx";
 import Footer from "../common/Footer";
-
 
 const Registro = ({ setUsuarioLogueado }) => {
   const {
@@ -50,83 +50,85 @@ const Registro = ({ setUsuarioLogueado }) => {
 
   return (
     <>
-    <Container className="mainSection d-block align-items-center justify-content-center p-3 my-5">
-      <Card className="my-5">
-        <Card.Header className="text-center titulo py-3" as="h3">
-          Registro
-        </Card.Header>
-        <Card.Body className="texto_general">
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group className="mb-2">
-              <Form.Label>Usuario</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ingrese un nombre de usuario"
-                {...register("nombre", {
-                  required: "El nombre  es un dato obligatorio",
-                })}
-              />
-            </Form.Group>
-            <Form.Group className="mb-2">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                placeholder="Ingrese un email"
-                type="email"
-                {...register("email", {
-                  required: "El email es un dato obligatorio",
-                  pattern: {
-                    value:
-                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-                    message:
-                      "El email debe cumplir con el formato mail@dominio.com",
-                  },
-                })}
-              />
-            </Form.Group>
-            <Form.Group className="mb-2">
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Ingrese una contraseña"
-                {...register("password", {
-                  required: "La contraseña es un dato obligatorio",
-                  pattern: {
-                    value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
-                    message:
-                      "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.",
-                  },
-                })}
-              />
-            </Form.Group>
+      <CustomNav usuarioLogueado="" setUsuarioLogueado=""></CustomNav>
 
-            <div className="d-flex justify-content-center">
-              <Button className="botonIngresar px-3 my-3" type="submit">
-                Registrar
+      <Container className="mainSection d-block align-items-center justify-content-center p-3 my-5">
+        <Card className="my-5">
+          <Card.Header className="text-center titulo py-3" as="h3">
+            Registro
+          </Card.Header>
+          <Card.Body className="texto_general">
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group className="mb-2">
+                <Form.Label>Usuario</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese un nombre de usuario"
+                  {...register("nombre", {
+                    required: "El nombre  es un dato obligatorio",
+                  })}
+                />
+              </Form.Group>
+              <Form.Group className="mb-2">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  placeholder="Ingrese un email"
+                  type="email"
+                  {...register("email", {
+                    required: "El email es un dato obligatorio",
+                    pattern: {
+                      value:
+                        /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                      message:
+                        "El email debe cumplir con el formato mail@dominio.com",
+                    },
+                  })}
+                />
+              </Form.Group>
+              <Form.Group className="mb-2">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ingrese una contraseña"
+                  {...register("password", {
+                    required: "La contraseña es un dato obligatorio",
+                    pattern: {
+                      value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                      message:
+                        "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.",
+                    },
+                  })}
+                />
+              </Form.Group>
+
+              <div className="d-flex justify-content-center">
+                <Button className="botonIngresar px-3 my-3" type="submit">
+                  Registrar
+                </Button>
+              </div>
+            </Form>
+          </Card.Body>
+          <Card.Footer className="texto_general">
+            <div
+              className="d-flex justify-content-center mx-auto "
+              style={{ width: "40%" }}
+            >
+              <Button className="m-1 botonesIconos">
+                <AiFillFacebook size="20" />
+              </Button>
+
+              <Button className="m-1 botonesIconos">
+                <AiOutlineTwitter size="20" />
+              </Button>
+
+              <Button className="m-1 botonesIconos">
+                <AiFillInstagram size="20" />
               </Button>
             </div>
-          </Form>
-        </Card.Body>
-        <Card.Footer className="texto_general">
-          <div
-            className="d-flex justify-content-center mx-auto "
-            style={{ width: "40%" }}
-          >
-            <Button className="m-1 botonesIconos">
-              <AiFillFacebook size="20" />
-            </Button>
-
-            <Button className="m-1 botonesIconos">
-              <AiOutlineTwitter size="20" />
-            </Button>
-
-            <Button className="m-1 botonesIconos">
-              <AiFillInstagram size="20" />
-            </Button>
-          </div>
-        </Card.Footer>
-      </Card>
-    </Container>
-    <Footer></Footer>
+          </Card.Footer>
+        </Card>
+      </Container>
+      <Footer></Footer>
     </>
   );
 };
