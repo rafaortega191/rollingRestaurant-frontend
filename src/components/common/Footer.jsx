86dc3ecbbf705e638fdecad5609b2eaf4675cd45
Eugenia Caceres <eugeniacaceres.mh@gmail.com>
2023-08-01 02:43:05 -0300

    centre para dispositivos pequeños

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Table,Button } from "react-bootstrap";
+import { Container, Row, Col, Table, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Footer.css";
 import {
@@ -14,42 +14,36 @@ const Footer = () => {
       <Container>
         <Row>
           <Col lg={6} md={12} mb={4}>
-            <h5 className="my-3 color enlaces" style={{ letterSpacing: "2px",}}>
+            <h5 className="my-3 color enlaces text-center text-sm-start" style={{ letterSpacing: "2px" }}>
               Roling Restaurant
             </h5>
-
-            <h6 className="mb-3 h6-footer" style={{ letterSpacing: "2px",}}>
+            <h6 className="mb-3 h6-footer text-center text-sm-start" style={{ letterSpacing: "2px" }}>
               ¡Sabor italiano en cada bocado! Descubre la pasión de nuestra pasta online!
             </h6>
-            <div
-              className="d-flex justify-content-center mx-auto "
-              style={{ width: "40%" }}
-            >
+            <div className="d-flex justify-content-center mx-auto">
               <Button className="m-1 botonesIconos">
-                <AiFillFacebook size="20" />
+                <AiFillFacebook size="20" to="/Error404" />
               </Button>
-
               <Button className="m-1 botonesIconos">
                 <AiOutlineTwitter size="20" />
               </Button>
-
               <Button className="m-1 botonesIconos">
                 <AiFillInstagram size="20" />
               </Button>
             </div>
           </Col>
           <Col lg={2} md={6} mb={4}>
-            <h5 className="my-3 titulo-footer color" style={{ letterSpacing: "2px", }}>
+            <h5 className="my-3 titulo-footer color text-center text-sm-start" style={{ letterSpacing: "2px" }}>
               Sitios
             </h5>
-            <ul className="list-unstyled mb-0">
+            <ul className="list-unstyled mb-0 text-center text-sm-start">
               <li className="mb-1">
-                <Link to="/registro" className="color enlaces"> 
+                <Link to="/registro" className="color enlaces">
                   Registro
                 </Link>
               </li>
               <li className="mb-1">
-                <Link to="/login" className="color enlaces" >
+                <Link to="/login" className="color enlaces">
                   Login
                 </Link>
               </li>
@@ -59,17 +53,16 @@ const Footer = () => {
                 </Link>
               </li>
               <li>
-                <Link to="/acercadenosotros"className="color enlaces">
+                <Link to="/acercadenosotros" className="color enlaces">
                   Nosotros
                 </Link>
               </li>
             </ul>
           </Col>
           <Col lg={4} md={6} mb={4}>
-            <h5 className="my-3 titulo-footer color"  style={{ letterSpacing: "2px"}}>
+            <h5 className="my-3 titulo-footer color text-center text-sm-start" style={{ letterSpacing: "2px" }}>
               Horarios
             </h5>
-            
             <Table style={{ color: "#4f4f4f", borderColor: "#666" }}>
               <tbody>
                 <tr>
@@ -88,7 +81,6 @@ const Footer = () => {
             </Table>
           </Col>
         </Row>
-        
       </Container>
 
       <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
