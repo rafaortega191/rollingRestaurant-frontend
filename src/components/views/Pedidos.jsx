7a6950038e7f87e4088ef8ec9357b36c82539ef8
rafaortega191 <rafaelortega1911@gmail.com>
2023-08-28 02:26:42 -0300

    pedidos
    cambio en la estructura del if para mostrar si el usuario esta loggeado o si cargo productos en su carrito y se arreglo que las cards en la pagina principal no se mostraran ordenadas

--- src/components/views/Pedidos.jsx
+++ src/components/views/Pedidos.jsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
-import Footer from '../common/Footer';
-import CustomNav from '../common/CustomNav';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import Footer from "../common/Footer";
+import CustomNav from "../common/CustomNav";
+import { Link, useNavigate } from "react-router-dom";
 import { format } from "date-fns";
 import { v4 as uuidv4 } from "uuid";
-import { consultaAgregarPedido } from '../helpers/queries';
-import Swal from 'sweetalert2';
+import { consultaAgregarPedido } from "../helpers/queries";
+import Swal from "sweetalert2";
+import "./Pedidos.css";
 
-const Pedidos = ({usuarioLogeado, setUsuarioLogeado}) => {
+const Pedidos = ({ usuarioLogeado, setUsuarioLogeado }) => {
   const [productosSeleccionados, setProductosSeleccionados] = useState([]);
+  const navegacion = useNavigate();
 
   useEffect(() => {
     // Obtener los productos seleccionados del Local Storage
     const productosJSON = localStorage.getItem("productosSeleccionados");
     const productos = JSON.parse(productosJSON) || [];
-    
+
     setProductosSeleccionados(productos);
   }, []);
 
@@ -23,36 +25,43 @@ const Pedidos = ({usuarioLogeado, setUsuarioLogeado}) => {
     const nuevosProductos = [...productosSeleccionados];
     nuevosProductos.splice(indice, 1);
     setProductosSeleccionados(nuevosProductos);
-    localStorage.setItem("productosSeleccionados", JSON.stringify(nuevosProductos));
+    localStorage.setItem(
+      "productosSeleccionados",
+      JSON.stringify(nuevosProductos)
+    );
   };
 
   const handleCantidadChange = (indice, cantidad) => {
     const nuevosProductos = [...productosSeleccionados];
     nuevosProductos[indice].cantidad = cantidad;
     setProductosSeleccionados(nuevosProductos);
-    localStorage.setItem("productosSeleccionados", JSON.stringify(nuevosProductos));
+    localStorage.setItem(
+      "productosSeleccionados",
+      JSON.stringify(nuevosProductos)
+    );
   };
 
-    const calcularPrecioTotal = () => {
+  const calcularPrecioTotal = () => {
     let total = 0;
     productosSeleccionados.forEach((producto) => {
       total += producto.precio * (producto.cantidad || 1);
     });
     return total;
   };
-  
+
   const handleCompra = () => {
     const nombreUsuario = sessionStorage.getItem("usuario");
     const fechaActual = format(new Date(), "yyyy-MM-dd");
-    const pedidoId = uuidv4().replace(/-/g, '');
-  
-    const productosLocalStorage = JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
-  
+    const pedidoId = uuidv4().replace(/-/g, "");
+
+    const productosLocalStorage =
+      JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
+
     const productosPendientes = {
       _id: pedidoId,
       fecha: fechaActual,
       usuario: nombreUsuario,
-      productos: productosLocalStorage.map(producto => ({
+      productos: productosLocalStorage.map((producto) => ({
         nombreProducto: producto.nombreProducto,
         cantidad: producto.cantidad,
         precio: producto.precio,
@@ -63,79 +72,104 @@ const Pedidos = ({usuarioLogeado, setUsuarioLogeado}) => {
       estado: "pendiente",
       precioTotal: calcularPrecioTotal(productosLocalStorage), // Asegúrate de tener la función calcularPrecioTotal definida
     };
-  
+
     consultaAgregarPedido(productosPendientes).then((respuesta) => {
-      if (respuesta && sessionStorage.getItem("usuario") && localStorage.getItem("productosSeleccionados")) {
-        Swal.fire(
-          "¡Compra realizada!",
-          "La compra se ha realizado exitosamente. Muchas Gracias.",
-          "success"
-        );
-  
-        localStorage.removeItem("productosSeleccionados");
-        
-      } else {
-        Swal.fire(
-          "Ocurrió un error",
-          "Intente esta operación en unos minutos",
-          "error"
-        );
+      if (respuesta) {
+        if (sessionStorage.getItem("usuario") && localStorage.getItem("productosSeleccionados")) {
+          // Usuario loggeado y productos en el carrito
+          Swal.fire(
+            "¡Compra realizada!",
+            "La compra se ha realizado exitosamente. Muchas Gracias.",
+            "success"
+          );
+      
+          localStorage.removeItem("productosSeleccionados");
+          navegacion("/");
+        } else if (!sessionStorage.getItem("usuario")) {
+          // Usuario no loggeado
+          Swal.fire(
+            "¡Error!",
+            "Debes iniciar sesión para realizar una compra.",
+            "error"
+          );
+        } else {
+          // Usuario loggeado pero sin productos en el carrito
+          Swal.fire(
+            "¡Error!",
+            "Tu carrito está vacío. Agrega productos antes de realizar una compra.",
+            "error"
+          );
+        }
       }
+      
     });
-    
   };
-  
-  
-  
-  
 
   return (
     <section>
-      <CustomNav 
-      usuarioLogeado={usuarioLogeado}
-      setUsuarioLogeado={setUsuarioLogeado}
+      <CustomNav
+        usuarioLogeado={usuarioLogeado}
+        setUsuarioLogeado={setUsuarioLogeado}
       ></CustomNav>
-    <Container>
-      <h2>Tus Pedidos</h2>
-      <div className="row">
-        {productosSeleccionados.map((producto, index) => (
-          <div className="col-md-4" key={index}>
-            <div className="card mb-4">
-              <img src={producto.imagen} className="card-img-top" alt={producto.nombreProducto} />
-              <div className="card-body">
-                <h5 className="card-title">{producto.nombreProducto}</h5>
-                <p className="card-text">Precio: {producto.precio}</p>
-                <p className="card-text">Categoría: {producto.categoria}</p>
-                <p className="card-text">{producto.descripcion}</p>
-                {/* Otros detalles del producto */}
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleEliminarClick(index)}
-                >
-                  Eliminar
-                </button>
-                <br />
-                {/* Agregar el campo de entrada para la cantidad */}
-                <label>Cantidad:</label>
-                  <input
-                    type="number"
-                    value={producto.cantidad || 1}
-                    onChange={(e) => handleCantidadChange(index, parseInt(e.target.value))}
-                    className="form-control"
-                    min="1"
-                  />
+      <Container>
+        <h1 className="h1-titulo text-center m-4">Tus Pedidos</h1>
+        <hr />
+        <div className="row card-pedidos-container rounded-4">
+          {productosSeleccionados.map((producto, index) => (
+            <div className="col-md-4 d-flex mt-3" key={index}>
+              <div className="card mb-4 d-flex flex-column">
+                <img
+                  src={producto.imagen}
+                  className="card-img-top"
+                  alt={producto.nombreProducto}
+                />
+                <div className="card-body d-flex flex-column">
+                  <div>
+                    <h5 className="card-title">{producto.nombreProducto}</h5>
+                    <p className="card-text">Precio: {producto.precio}</p>
+                    <p className="card-text">Categoría: {producto.categoria}</p>
+                    <p className="card-text">{producto.descripcion}</p>
+                  </div>
+                  <div className="mt-auto">
+                    <button
+                      className="btn btn-danger mt-4 mb-2"
+                      onClick={() => handleEliminarClick(index)}
+                    >
+                      Eliminar
+                    </button>
+                    <br />
+                    <label>Cantidad:</label>
+                    <input
+                      type="number"
+                      value={producto.cantidad || 1}
+                      onChange={(e) =>
+                        handleCantidadChange(index, parseInt(e.target.value))
+                      }
+                      className="form-control"
+                      min="1"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <h4>Precio Total: {calcularPrecioTotal()}</h4>
-      <br />
-      <button className="btn btn-danger" onClick={handleCompra}>COMPRAR</button>
-      <br />
-      <Link className="btn btn-danger mt-2" to="/">Volver a inicio</Link>
-    </Container>
-    <Footer></Footer>
+          ))}
+        </div>
+        <div className="mt-5 pago-container w-25 rounded-3">
+          <h4 className="text-center pago-texto">
+            Precio Total: {calcularPrecioTotal()}
+          </h4>
+
+          <button className="btn btn-danger m-2" onClick={handleCompra}>
+            COMPRAR
+          </button>
+          <br />
+          <Link className="btn btn-danger m-2" to="/">
+            Volver a inicio
+          </Link>
+        </div>
+      </Container>
+
+      <Footer></Footer>
     </section>
   );
 };
--- src/components/views/producto/CardProducto.jsx
+++ src/components/views/producto/CardProducto.jsx
@@ -1,57 +1,74 @@
 import { Col, Card } from "react-bootstrap";
-import { Link,useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import React from "react";
 import "../paginaprincipal.css";
 import Swal from "sweetalert2";
 
-const CardProducto = ({ user,producto }) => {
+const CardProducto = ({ user, producto }) => {
   const navigate = useNavigate();
-  console.log(user)
+  console.log(user);
   const handleComprarClick = () => {
     if (user === null) {
       navigate("/login");
     } else {
       const productoJSON = JSON.stringify(producto);
-      const productosSeleccionados = JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
-  
+      const productosSeleccionados =
+        JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
+
       // Verificar si el producto ya está en el carrito
-      const productoExistente = productosSeleccionados.find(item => item._id === producto._id);
-  
+      const productoExistente = productosSeleccionados.find(
+        (item) => item._id === producto._id
+      );
+
       if (productoExistente) {
         // Mostrar mensaje de error usando SweetAlert2
         Swal.fire({
-          icon: 'error',
-          title: 'Producto ya en el carrito',
-          text: 'Este producto ya ha sido agregado al carrito.',
+          icon: "error",
+          title: "Producto ya en el carrito",
+          text: "Este producto ya ha sido agregado al carrito.",
         });
       } else {
         productosSeleccionados.push(producto);
-        localStorage.setItem("productosSeleccionados", JSON.stringify(productosSeleccionados));
+        localStorage.setItem(
+          "productosSeleccionados",
+          JSON.stringify(productosSeleccionados)
+        );
         navigate(`/pedidos/${producto._id}`);
       }
     }
   };
-  
 
   return (
-    <Col md={3} ld={2} className="mb-3">
-      <Card className="card-principal-seleccionados">
-        <Card.Img className="imagen-card-seleccionadas" variant="top" src={producto.imagen} />
-        <Card.Body>
-          <Card.Title className="card-productos-titulo">{producto.nombreProducto}</Card.Title>
-          <Card.Text className="card-productos-precio">${producto.precio}</Card.Text>
-          <Link
-            className="btn card-productos-boton-sabermas"
-            to={`/detalles/${producto._id}`}
-          >
-            Ver más
-          </Link>
-          <Link
-            className="btn card-productos-boton-sabermas"
-            onClick={handleComprarClick} // Llamamos a la función al hacer clic en el botón "Comprar"
-          >
-            Comprar
-          </Link>
+    <Col md={4} ld={2} className="d-flex">
+      <Card className="card-principal-seleccionados d-flex">
+        <Card.Img
+          className="imagen-card-seleccionadas"
+          variant="top"
+          src={producto.imagen}
+        />
+        <Card.Body className="d-flex flex-column">
+          <div>
+            <Card.Title className="card-productos-titulo">
+              {producto.nombreProducto}
+            </Card.Title>
+          </div>
+          <div className="mt-auto">
+            <Card.Text className="card-productos-precio">
+              ${producto.precio}
+            </Card.Text>
+            <Link
+              className="btn card-productos-boton-sabermas"
+              to={`/detalles/${producto._id}`}
+            >
+              Ver más
+            </Link>
+            <Link
+              className="btn card-productos-boton-sabermas"
+              onClick={handleComprarClick}
+            >
+              Comprar
+            </Link>
+          </div>
         </Card.Body>
       </Card>
     </Col>
